4e416b07635e6cadae6e08d016604b3183f48d1f
Gavin Sharp <gavin@mozilla.com>
2009-03-17 17:52:02 -0400

    fix config_build.sh options to match reality

--- config_build.sh
+++ config_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Build config for build.sh
 APP_NAME=tabcloser
-CHROME_PROVIDERS="content locale skin"
+CHROME_PROVIDERS="content locale"
 CLEAN_UP=1
 ROOT_FILES=
-ROOT_DIRS="defaults"
+#ROOT_DIRS="defaults"
 BEFORE_BUILD=
 AFTER_BUILD=
